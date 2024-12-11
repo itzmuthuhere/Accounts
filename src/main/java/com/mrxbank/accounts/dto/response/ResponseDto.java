f629c9d8d6ec0a8a8f6286611a630511cd03804e
itzmuthuhere <rajamuthu107@gmail.com>
2024-12-11 23:37:12 +0530

    accounts service implementation

---
+++ src/main/java/com/mrxbank/accounts/dto/response/ResponseDto.java
@@ -0,0 +1,29 @@
+package com.mrxbank.accounts.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response information"
+)
+@Data @AllArgsConstructor
+public class ResponseDto {
+
+
+
+    @Schema(
+            description = "Status code in the response"
+    )
+    private String statusCode;
+
+    @Schema(
+            description = "Status message in the response"
+    )
+    private String statusMsg;
+
+}
+
